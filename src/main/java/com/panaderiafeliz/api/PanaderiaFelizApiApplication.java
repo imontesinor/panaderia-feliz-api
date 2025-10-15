c0e164d40091fd770a359ec598abfbc1c2dcf52d
imontesinor <imontesinor@gmail.com>
2025-10-14 19:15:48 -0500

    chore: crear estructura base de paquetes del proyecto

---
+++ src/main/java/com/panaderiafeliz/api/PanaderiaFelizApiApplication.java
@@ -0,0 +1,13 @@
+package com.panaderiafeliz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaderiaFelizApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaderiaFelizApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panaderiafeliz/api/PanaderiaFelizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panaderiafeliz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanaderiaFelizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
